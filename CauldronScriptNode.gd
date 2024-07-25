80ace8fd157095d6838e6ff70f0c1c9c899713f7
Nicholas Dettmer <ruoyu100@163.com>
2024-07-25 09:00:02 +0200

    Removed corrupt assets and minor cleanup
    2 corrupt files:
    ...\assets\art\Icons\alchemical\background\37.png
    ...\assets\art\Icons\alchemical\without background\20.png

--- CauldronScriptNode.gd
+++ CauldronScriptNode.gd
@@ -2,7 +2,9 @@ extends CookingTool
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	recipes = {}
+	recipes = { 
+	"Pineapple" : ["Apple", "Pine Cone"]
+	}
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- room/room.gd
+++ room/room.gd
@@ -3,12 +3,13 @@ extends Node2D
 var cauldron_menu_scene = preload("res://cauldron_menu.tscn")
 var cauldron_menu
 
-@export var cauldron_button : TextureButton
+@export var cauldron_button : TextureButton	
 
 var active_menu : Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	cauldron_menu = cauldron_menu_scene.instantiate()
 	cauldron_button.add_child(cauldron_menu)
 	#cauldron_menu.global_position = cauldron_button.global_position
