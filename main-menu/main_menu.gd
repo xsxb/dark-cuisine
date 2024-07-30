01faf4053e91c6395bc35480431bdb7d051eecf6
Nicholas Dettmer <ruoyu100@163.com>
2024-07-30 15:06:41 +0200

    Main_menu spawners added

--- main-menu/main_menu.gd
+++ main-menu/main_menu.gd
@@ -2,16 +2,20 @@ extends Node2D
 
 @export var menu_container : VBoxContainer
 
-var room_scene
+var spawn_scene = preload("res://spawned_monster.tscn")
+var room_scene = preload("res://room/room.tscn")
 
 func _ready():
-	#Szenen vorladen:
-	room_scene = preload("res://room/room.tscn")
 	
 	#Menücontainer zentrieren an Fenstergröße angepasst:
 	menu_container.position = get_viewport_rect().size/2 - menu_container.size/2
 
-
-
 func _on_start_button_pressed():
 	get_tree().change_scene_to_packed(room_scene)
+
+func _on_spawn_timer_timeout():
+	var new_spawn = spawn_scene.instantiate()
+	get_parent().add_child(new_spawn)
+	new_spawn.global_position = Vector2(randi_range(50, get_viewport_rect().size.x), randi_range(50, get_viewport_rect().size.y))
+	
+	
