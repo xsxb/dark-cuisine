68ed28a5a0a11664ea81dd54d35e5eaef0457bde
Nicholas Dettmer <ruoyu100@163.com>
2024-07-30 12:24:39 +0200

    Added web with item respawn

---
+++ ui/fridge_inventory.gd
@@ -0,0 +1,14 @@
+extends Inventory
+
+
+
+func _ready():
+	init()
+
+	set_item(0,20)
+	set_item(1,24)
+	set_item(2,25)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
