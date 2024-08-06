8118cec148e229d6247e3f2383b073df816ba247
Lukas <62207660+Lukas-Lenz@users.noreply.github.com>
2024-08-06 15:56:18 +0200

    .

---
+++ cooking-system/recipes/recipe_master.gd
@@ -0,0 +1,35 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+# what tree structure?
+
+
+# reads in recipe and sorts into search tree
+
+
+# builds a tree of recipe references to check in order
+
+# i am a serious programmer hire me pls
+match_recipe_dom():
+	
+	match_recipe_sub(root, 0)
+
+match_recipe_sub(recipe : Recipe, depth : int) :
+	
+	if !recipe.
+	
+	for get
+
+#if matches this:
+  #call on ?each? child
+  #collect returns
+  #return deepest result or -1
