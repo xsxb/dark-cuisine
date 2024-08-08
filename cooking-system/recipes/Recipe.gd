e3aeeb46091236940f864fe30732db22463440f6
Liam <dana.lenz@protonmail.com>
2024-08-08 21:15:56 +0200

    added simple recipe class
    looks like i didnt push todays changes earlier

---
+++ cooking-system/recipes/Recipe.gd
@@ -0,0 +1,12 @@
+class_name Recipe extends Node
+
+var id
+var recipe_name
+var description
+
+
+
+func _init(id_, recipe_name_, description_):
+	id = id_
+	recipe_name = recipe_name_
+	description = description_
