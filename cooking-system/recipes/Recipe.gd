bb3ef797cdf50d78033bc58525a01d83ece78170
Lukas <62207660+Lukas-Lenz@users.noreply.github.com>
2024-08-09 16:02:26 +0200

    read in json

--- cooking-system/recipes/Recipe.gd
+++ cooking-system/recipes/Recipe.gd
@@ -1,12 +1,15 @@
 class_name Recipe extends Node
 
-var id
-var recipe_name
-var description
+var id : int
+var recipe_name : String
+var description : String
+var base_recipe : int
+var derived_recipes : Array[int]
 
 
-
-func _init(id_, recipe_name_, description_):
+func _init(id_, recipe_name_, description_, base_recipe_):
 	id = id_
 	recipe_name = recipe_name_
 	description = description_
+	base_recipe = 0
+	derived_recipes = []
--- cooking-system/recipes/recipe_master.gd
+++ cooking-system/recipes/recipe_master.gd
@@ -1,30 +1,71 @@
 extends Node
 
+#TODO: handle recipe ids per global enums to make int constants readable
+
+var rec_scene = load("res://cooking-system/recipes/Recipe.tscn")
+
+var recipes = {}
+
+func _init():
+	add_recipe_root(-1, "root", "uh, how did this turn up here?")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var path = "res://cooking-system/recipes/test.json"
+	var json_string = FileAccess.get_file_as_string(path)
+	load_from_json(json_string)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-var recipes = {}
 
 func get_recipe(rec_id) :
 	return recipes.get(rec_id)
 
 func get_children_ids(rec_id):
 	pass
+	
+
+
+func add_recipe_root(id, name, description):
+	add_recipe(id, name, description, null)
 
 # reads in recipe and sorts into search tree
-func load_recipe():
-	pass
+func add_recipe(id, name, description, base):
+	
+	if base == null && id != -1:
+		base = -1
+	
+	recipes[id] = rec_scene.instantiate()
+	recipes[id].id = id
+	recipes[id].name = name
+	recipes[id].description = description
+	recipes[id].base = base
+	
+	if base != null:
+		recipes.get(base).add_derived(id)
 
-func load_from_json():
-	pass
+
+func load_from_json(json_string : String):
+	
+	var json = JSON.new()
+	var error = json.parse(json_string)
+	if error == OK:
+		var rec_array = json.data
+		if typeof(rec_array) == TYPE_ARRAY:
+			print(rec_array) # Prints array
+			
+			for rec_object in rec_array:
+				var id = rec_object.get("ID")
+				var name = rec_object.get("name")
+				var description = rec_object.get("description")
+				var base = rec_object.get("base")
+				add_recipe(id, name, description, base)
+			
+			print(recipes)
+			
+		else:
+			print("Unexpected data")
+	else:
+		print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
+		pass
 
 # builds a tree of recipe references to check in order
 
