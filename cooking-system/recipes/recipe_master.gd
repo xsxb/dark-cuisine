a7bb9f413dfcd24ac24c184893f4e096a3b9c937
Lukas <62207660+Lukas-Lenz@users.noreply.github.com>
2024-08-09 10:18:39 +0200

    forgotten commit

--- cooking-system/recipes/recipe_master.gd
+++ cooking-system/recipes/recipe_master.gd
@@ -10,26 +10,39 @@ func _ready():
 func _process(delta):
 	pass
 
-# what tree structure?
 
+var recipes = {}
+
+func get_recipe(rec_id) :
+	return recipes.get(rec_id)
+
+func get_children_ids(rec_id):
+	pass
 
 # reads in recipe and sorts into search tree
+func load_recipe():
+	pass
 
+func load_from_json():
+	pass
 
 # builds a tree of recipe references to check in order
 
-# i am a serious programmer hire me pls
-match_recipe_dom():
-	
-	match_recipe_sub(root, 0)
 
-match_recipe_sub(recipe : Recipe, depth : int) :
-	
-	if !recipe.
-	
-	for get
+#func match_recipe_sub(recipe_id : Recipe, depth : int) :
+#	results = []
+#	for get_derived_recipes(recipe):
+#		if !recipe.	
+#	for get
 
 #if matches this:
   #call on ?each? child
   #collect returns
   #return deepest result or -1
+
+
+# i am a serious programmer hire me pls
+#func match_recipe_dom():
+#	match_recipe_sub(root, 0)
+
+
